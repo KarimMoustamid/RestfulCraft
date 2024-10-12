f69d2ab7378b99e1f570f12d6028b507fcdba6a2
karimmoustamid <karim.moustamid@agilethought.com>
2024-10-12 16:14:56 -0600

    Enable HTTP 406 responses and customize problem details
    Modified the controller options to return HTTP 406 (Not Acceptable) responses. Added customization to problem details to include the server name in the extension for better debugging.

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -2,7 +2,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.ReturnHttpNotAcceptable = true;
+});
+builder.Services.AddProblemDetails(options =>
+{
+    options.CustomizeProblemDetails = context =>
+    {
+        context.ProblemDetails.Extensions.Add("server", Environment.MachineName);
+    };
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
