db54b753d84a1714d45970b9c4239b885d465edc
karimmoustamid <karim.moustamid@agilethought.com>
2024-10-12 15:08:54 -0600

    Refactor city data retrieval to use CitiesDataStore
    Refactored `GetCities` method in `CitiesController` to retrieve city data from a new `CitiesDataStore` class. This change centralizes the data storage, making the code more maintainable and scalable. Dummy data has been initialized in `CitiesDataStore` to simulate real-world data retrieval.

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,6 +1,7 @@
 namespace CityInfo.API.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
+    using Models;
 
 
     [ApiController]
@@ -11,14 +12,7 @@ namespace CityInfo.API.Controllers
         [HttpGet]
         public JsonResult GetCities()
         {
-            return new JsonResult(new List<object>
-            {
-                new {id = 1 , Name = "Paris"},
-                new {id = 2 , Name = "New York"},
-                new {id = 3 , Name = "Tokyo"},
-                new {id = 4 , Name = "London"},
-                new {id = 5 , Name = "Sydney"}
-            });
+            return new JsonResult(CitiesDataStore.Current);
         }
     }
 }
