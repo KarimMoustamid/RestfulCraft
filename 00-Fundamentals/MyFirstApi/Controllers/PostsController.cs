f57edd93415b23f1ff4da4e0b795b1c9d099012c
karimmoustamid <moustamid.teck@gmail.com>
2025-01-07 20:00:22 -0600

    Add PostsService and integrate CRUD operations in controller
    Introduced a new PostsService to handle in-memory CRUD logic for posts. Updated PostsController to utilize this service, enabling functionality for creating, retrieving, updating, and deleting posts via API endpoints. This establishes a foundation for future enhancements.

--- 00-Fundamentals/MyFirstApi/Controllers/PostsController.cs
+++ 00-Fundamentals/MyFirstApi/Controllers/PostsController.cs
@@ -2,11 +2,19 @@ namespace MyFirstApi.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
     using Models;
+    using Services;
 
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/posts")]
     public class PostsController : ControllerBase
     {
+        private readonly PostsService _postsService;
+
+        public PostsController()
+        {
+            _postsService = new PostsService();
+        }
+
         [HttpGet]
         public ActionResult<IEnumerable<Post>> GetPosts()
         {
@@ -34,6 +42,42 @@ namespace MyFirstApi.Controllers
                     Body = "Post 3 body"
                 },
             };
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Post>> GetPost(int id)
+        {
+            var post = await _postsService.GetPost(id);
+            if (post == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.Ok(post);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Post>> CreatePost(Post item)
+        {
+            await _postsService.CreatePost(item);
+            return this.CreatedAtAction(nameof(GetPost), new {id = item.Id}, item);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<Post>> UpdatePost(int id, Post item)
+        {
+            if (id != item.Id)
+            {
+                return this.BadRequest();
+            }
+
+            var updatedPost = await _postsService.UpdatePost(id, item);
+            if (updatedPost == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
         }
     }
 }
